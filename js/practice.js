23629c13d2be262739e54debdaaf50d104cd110d
Chaerim Park <dasapcr@gmail.com>
2022-07-15 18:19:44 +0900

    0715 upload

--- js/practice.js
+++ js/practice.js
@@ -34,5 +34,16 @@ function tabBtn(a){
     });
 }
 
+const input = prompt();
+
+if (!input){
+    console.log("입력값이 없습니다.");
+} else {
+    console.log(input);
+}
+
+console.log(input || "입력값이 없습니다.");
+
+
 
 
